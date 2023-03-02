ce66cb200e49410e8415caa260c0d15191206b37
kimhyeji <gpwl5589@gmail.com>
2023-03-02 21:44:49 +0900

    데이터 리스트 조회

--- db.sql
+++ db.sql
@@ -1,7 +1,9 @@
+# DB 생성
 DROP DATABASE IF EXISTS wise_saying;
 CREATE DATABASE wise_saying;
 USE wise_saying;
 
+# 테이블 생성
 CREATE TABLE wise_saying (
     id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     regDate DATETIME NOT NULL,
@@ -9,6 +11,7 @@ CREATE TABLE wise_saying (
     author VARCHAR(50) NOT NULL
 );
 
+# 데이터 생성
 INSERT INTO wise_saying
 SET regDate = NOW(),
 content = '나는 의적이다',
