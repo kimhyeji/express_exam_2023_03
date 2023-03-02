b01d9535a86be168ab114a9e97ae9d53a5c56062
kimhyeji <gpwl5589@gmail.com>
2023-03-02 21:22:29 +0900

    DB 구현

---
+++ db.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS wise_saying;
+CREATE DATABASE wise_saying;
+USE wise_saying;
+
+CREATE TABLE wise_saying (
+    id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    regDate DATETIME NOT NULL,
+    content VARCHAR(200) NOT NULL,
+    author VARCHAR(50) NOT NULL
+);
+
+INSERT INTO wise_saying
+SET regDate = NOW(),
+content = '나는 의적이다',
+author = '홍길동';
+
+INSERT INTO wise_saying
+SET regDate = NOW(),
+content = '나는 도적이다',
+author = '임꺽정';
+
+INSERT INTO wise_saying
+SET regDate = NOW(),
+content = '나도 도적이다',
+author = '홍길순';
